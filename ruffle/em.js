3148e5f1c961258a5799034f227f8042640cf75d
idkmod12 <idezmonsp@gmail.com>
2026-09-17 18:29:32 -0400

    Host Ruffle locally for Flash games

---
+++ ruffle/em.js
@@ -0,0 +1,32 @@
+var thisScript = document.currentScript;
+var file = thisScript.getAttribute("data-swf-file-src");
+var name = thisScript.getAttribute("data-fname") || thisScript.getAttribute("data-fName") || "Flash game";
+var targetUrl = new URL(file, window.location.href).href;
+document.getElementById("gameFName").textContent = name;
+window.RufflePlayer = window.RufflePlayer || {};
+window.RufflePlayer.config = {
+  autoplay: "off",
+  contextMenu: "off",
+  splashScreen: false,
+  openUrlMode: "confirm",
+  favorFlash: true,
+  playerRuntime: "flashPlayer",
+  allowFullscreen: true,
+  publicPath: "/ruffle/"
+};
+window.PlayerObject = null;
+window.addEventListener("load", function () {
+  var ruffle = window.RufflePlayer && window.RufflePlayer.newest && window.RufflePlayer.newest();
+  if (!ruffle) return;
+  var player = ruffle.createPlayer();
+  player.style.width = "800px";
+  player.style.height = "600px";
+  var container = document.getElementById("gameContainer");
+  container.replaceChildren(player);
+  window.PlayerObject = player;
+  player.load(targetUrl);
+});
+var fullscreenButton = document.getElementById("fullscreen");
+if (fullscreenButton) fullscreenButton.addEventListener("click", function () {
+  if (window.PlayerObject && window.PlayerObject.enterFullscreen) window.PlayerObject.enterFullscreen();
+});
